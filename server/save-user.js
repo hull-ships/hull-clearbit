1c5d7599815191ead29b62123262d2e96503cf8a
Romain Dardour <romain@hull.io>
2016-02-28 00:43:18 +0100

    formatting

--- server/save-user.js
+++ server/save-user.js
@@ -2,10 +2,9 @@ import _ from 'lodash';
 import map from './clearbit-mapping';
 import moment from 'moment';
 
-export default function( {hull, user={}, person={} } ){
-
+export default function({ hull, user = {}, person = {} }) {
   const { props, traits } = map(person);
-  traits.cb_fetched_at = moment().format()
+  traits.cb_fetched_at = moment().format();
   const hullUser = hull.as(user.id);
   return hullUser
   .put('me', _.omit(props, _.keys(user)))
