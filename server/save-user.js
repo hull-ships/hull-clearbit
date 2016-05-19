b38bb3fe6d8f1df04eba993db987b689ea76c3e2
Stephane Bellity <sbellity@gmail.com>
2016-05-19 11:35:52 +0200

    typo

--- server/save-user.js
+++ server/save-user.js
@@ -39,7 +39,7 @@ export default function({ hull, user = {}, person = {} }) {
     traits['clearbit/fetched_at'] = new Date().toISOString();
     return hull.as(user.id).traits(traits);
   } else {
-    hull.utils.log("[user.skip] no changes", user.id, JSON.stringify({ person, user });
+    hull.utils.log("[user.skip] no changes", user.id, JSON.stringify({ person, user }));
   }
 
 }
