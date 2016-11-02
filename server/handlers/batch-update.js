1ea2a9f128070141c13cf4483471fe2340f09ea0
Stephane Bellity <stephane@hull.io>
2016-11-02 22:38:41 +0100

    Bottleneck

--- server/handlers/batch-update.js
+++ server/handlers/batch-update.js
@@ -1,14 +1,33 @@
+import Bottleneck from "bottleneck";
 import userUpdateHandler from "./user-update";
+
+const Limiter = new Bottleneck.Cluster(3);
+
+setInterval(() => {
+  Limiter.all((limiter, key) => {
+    const nbRunning = limiter.nbRunning();
+    const nbQueued = limiter.nbQueued();
+    if (nbRunning || nbQueued) {
+      console.warn("Limiter", JSON.stringify({
+        nbRunning,
+        nbQueued
+      }));
+
+    }
+  });
+}, 1000);
 
 export default function handleBatchUpdate({ hostSecret }) {
   const handleUserUpdate = userUpdateHandler({
-    hostSecret, stream: true, forceFetch: true
+    hostSecret, stream: false, forceFetch: true
   });
   return (messages = [], context = {}) => {
-    const { hull, processed } = context;
+    const { hull, ship, processed } = context;
+    const limiter = Limiter.key(ship.id);
     hull.logger.info("processing batch", { processed });
+    const handleMessage = (m) => handleUserUpdate(m, context)
     return messages.map(
-      m => handleUserUpdate(m, context)
+      m => limiter.schedule(handleMessage, m)
     );
   };
 }
