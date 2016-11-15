c32ff48ffb6ab8a65f3f2c47ed538d4206df360e
Stephane Bellity <stephane@hull.io>
2016-11-15 11:11:43 +0100

    Middleware signature

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ module.exports = function Server(options = {}) {
 
   app.post("/prospect",
     bodyParser.urlencoded(),
-    Middlewares.hullClient({ hostSecret }),
+    hullClient({ hostSecret }),
     handleProspect(options)
   );
 
