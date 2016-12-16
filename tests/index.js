11673fe217c7ca9d16e9206aab316136543741ac
Stephane Bellity <stephane@hull.io>
2016-12-16 18:19:48 +0100

    add tests for mapping

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,25 @@
+/* eslint-env node, mocha */
+
 require("babel-register")({ presets: ["es2015", "stage-0"] });
+const assert = require("assert");
+const _ = require("lodash");
+const { getUserTraitsFromPerson } = require("../server/clearbit/mapping");
+
+const reveal = require("./fixtures/reveal.json");
+const combined = require("./fixtures/combined.json");
 
 describe("HullClearbit Client", () => {
-  it("should work fine", () => {
+  it("Should return alexa rank from reveal payload", () => {
+    const traits = getUserTraitsFromPerson({ user: {}, person: reveal });
+    const rank = _.get(reveal, "company.metrics.alexaGlobalRank");
+    assert.equal(rank, 30726);
+    assert.equal(traits["clearbit_company/metrics_alexa_global_rank"], rank);
+  });
 
+  it("Should return alexa rank from combined payload", () => {
+    const traits = getUserTraitsFromPerson({ user: {}, person: combined });
+    const rank = _.get(reveal, "company.metrics.alexaGlobalRank");
+    assert.equal(rank, 30726);
+    assert.equal(traits["clearbit_company/metrics_alexa_global_rank"], rank);
   });
 });
