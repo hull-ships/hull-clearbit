6a54d0378a6202489b0951902fe28cbf83e6d5d9
Thomas Kgaevski <thomas@hull.io>
2017-04-25 15:34:25 +0200

    save accounts only if feature flag is enabled

--- server/clearbit.js
+++ server/clearbit.js
@@ -111,25 +111,29 @@ export default class Clearbit {
     this.metric("saveUser");
     this.log("incoming.user.success", { traits, source, external_id, id, email });
 
-    const all_traits = { user: {}, account: {} };
+    if (this.settings.handle_accounts) {
+      const all_traits = { user: {}, account: {} };
 
-    _.map(traits, (v, k) => {
-      const [group, trait] = k.split("/");
-      if (group === "clearbit_company") {
-        all_traits.account[`clearbit/${trait}`] = v;
-      } else {
-        all_traits.user[k] = v;
+      _.map(traits, (v, k) => {
+        const [group, trait] = k.split("/");
+        if (group === "clearbit_company") {
+          all_traits.account[`clearbit/${trait}`] = v;
+        } else {
+          all_traits.user[k] = v;
+        }
+      }, {});
+
+      const domain = all_traits.account["clearbit_company/domain"];
+
+      const client = this.hull.asUser(ident);
+
+      client.traits(all_traits.user);
+
+      if (domain) {
+        client.account({ domain }).traits(all_traits.account);
       }
-    }, {});
-
-    const domain = all_traits.account["clearbit_company/domain"];
-
-    const client = this.hull.asUser(ident);
-
-    client.traits(all_traits.user);
-
-    if (domain) {
-      client.account({ domain }).traits(all_traits.account);
+    } else {
+      this.hull.asUser(ident).traits(traits);
     }
 
     return Promise.resolve({ user, person });
