a46f3d455724dcce0c013ef019131d28c7880d3a
Romain Dardour <romain@hull.io>
2018-01-24 13:05:23 +0100

    cleanups

--- server/lib/user-update-logic.js
+++ server/lib/user-update-logic.js
@@ -2,37 +2,30 @@ export default function userUpdateLogic({ message = {}, clearbit, client }) {
   const { segments, user, account } = message;
   const acc = account || user.account;
 
+  client.asUser(user).logger.info("outgoing.user.start");
   if (clearbit.canEnrich(user)) {
-    client
-      .asUser(user)
-      .logger.info("outgoing.user.start", { action: "enrich" });
     if (clearbit.shouldEnrich(message)) {
       return clearbit.enrichUser(user);
     }
   }
 
   if (clearbit.canReveal(user)) {
-    client
-      .asUser(user)
-      .logger.info("outgoing.user.start", { action: "reveal" });
     if (clearbit.shouldReveal(message)) {
       return clearbit.revealUser(user);
     }
   }
 
   if (clearbit.shouldDiscover(message)) {
-    client
-      .asUser(user)
-      .logger.info("outgoing.user.start", { action: "discover" });
     return clearbit.discoverSimilarCompanies(user);
   }
 
   if (clearbit.shouldProspect({ segments, user, account: acc })) {
-    client
-      .asUser(user)
-      .logger.info("outgoing.user.start", { action: "prospect" });
     return clearbit.prospectUsers(user, acc);
   }
+
+  client
+    .asUser(user)
+    .logger.info("outgoing.user.skip", { reason: "no action matched" });
 
   return false;
 }
